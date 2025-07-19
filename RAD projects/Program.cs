45911015f7425d2a531ef40635b42ca93bc7dd20
mari75a <sangeethlakshan0@gmail.com>
2025-07-19 14:54:56 +0530

    add centerd triangle

--- RAD projects/Program.cs
+++ RAD projects/Program.cs
@@ -35,8 +35,26 @@ namespace RAD_projects
             }
 
             printStars(num);
+            printMidTriangle(num);
         }
 
-        
+         public static void printMidTriangle(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = n - 1; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k <= i; k++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+
     }
 }
